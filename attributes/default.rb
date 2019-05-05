cbfecbbd66434f02c3510539d333b4b77509b8b5
DacDo <mr.dacdo@gmail.com>
2019-05-05 23:16:23 +1000

    styled

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,29 +1,31 @@
-default['audit']['fetcher'] = 'chef-automate'
+# default['audit']['fetcher'] = 'chef-automate'
 default['audit']['reporter'] = 'chef-automate'
+# default['audit']['reporter'] = 'json-file'
 default['audit']['server'] = 'https://13.210.69.80/data-collector/v0/'
 default['audit']['refresh_token'] = 'God3ytMQm6w4QbbdrFJGyEvEFZY='
+default['audit']['token'] = 'God3ytMQm6w4QbbdrFJGyEvEFZY='
 
 default['audit']['profiles'].push(
   #  Profile from Chef Compliance
-  {
-    'name': 'linux',
-    'compliance': 'base/linux'
-  },
+  # {
+  #   'name': 'linux',
+  #   'compliance': 'base/linux'
+  # },
   # Profile from Supermarket
   # note: If reporting to Compliance, first upload the Supermarket profile to Chef Compliance.
   # note: Artifactory's Supermarket implementation—"Chef Cookbook repository"—does not support InSpec compliance profiles at this time
   {
     'name': 'ssh',
-    'supermarket': 'hardening/ssh-hardening'
+    'supermarket': 'hardening/ssh-hardening',
   },
   # Profile from GitHub
   {
     'name': 'ssl',
-    'git': 'https://github.com/dev-sec/ssl-benchmark.git'
+    'git': 'https://github.com/dev-sec/ssl-benchmark.git',
   },
   # Profile from URL
   {
     'name': 'ssh',
-    'url': 'https://github.com/dev-sec/tests-ssh-hardening/archive/master.zip'
+    'url': 'https://github.com/dev-sec/tests-ssh-hardening/archive/master.zip',
   }
 )
--- policyfiles/my_audit.rb
+++ policyfiles/my_audit.rb
@@ -1,5 +1,5 @@
 name 'my_audit'
-run_list "recipe[my_audit::default]"
-cookbook "my_audit", path: '..'
+run_list 'recipe[my_audit::default]'
+cookbook 'my_audit', path: '..'
 cookbook 'audit', path: '../../audit'
 default_source :supermarket
