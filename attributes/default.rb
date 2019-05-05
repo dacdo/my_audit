344579491d80eb7d659dba00ba5598ed58e8200a
DacDo <mr.dacdo@gmail.com>
2019-05-05 22:46:55 +1000

    added profiles

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,27 @@ default['audit']['reporter'] = 'chef-automate'
 default['audit']['server'] = 'https://13.210.69.80/data-collector/v0/'
 default['audit']['refresh_token'] = 'God3ytMQm6w4QbbdrFJGyEvEFZY='
 
-default['audit']['profiles'] = [
+default['audit']['profiles'].push(
+  #  Profile from Chef Compliance
   {
-    name: 'CIS Ubuntu Linux 16.04 LTS Benchmark Level 1 - Server',
-    compliance: 'admin/cis-ubuntu16.04lts-level1-server',
+    'name': 'linux',
+    'compliance': 'base/linux'
   },
-]
+  # Profile from Supermarket
+  # note: If reporting to Compliance, first upload the Supermarket profile to Chef Compliance.
+  # note: Artifactory's Supermarket implementation—"Chef Cookbook repository"—does not support InSpec compliance profiles at this time
+  {
+    'name': 'ssh',
+    'supermarket': 'hardening/ssh-hardening'
+  },
+  # Profile from GitHub
+  {
+    'name': 'ssl',
+    'git': 'https://github.com/dev-sec/ssl-benchmark.git'
+  },
+  # Profile from URL
+  {
+    'name': 'ssh',
+    'url': 'https://github.com/dev-sec/tests-ssh-hardening/archive/master.zip'
+  }
+)
